670bbb91ea6fc829f67031b5d28395a2714842d7
Timothy Baney <timbaney1989@gmail.com>
2016-11-08 00:06:20 -0600

    add single day weather component

---
+++ src/app/weather-day/weather-day.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { WeatherDayComponent } from './weather-day.component';
+
+describe('Component: WeatherDay', () => {
+  it('should create an instance', () => {
+    let component = new WeatherDayComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/weather-day/weather-day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-day',
+  templateUrl: './weather-day.component.html',
+  styleUrls: ['./weather-day.component.css']
+})
+export class WeatherDayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
